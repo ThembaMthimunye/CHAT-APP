5f5db56f866277dbd0ee8e291ab7754e5e7751ca
ThembaMthimunye <218634567@tut4life.ac.za>
2025-02-06 10:38:14 +0200

    Themba

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import { useState } from "react";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -11,10 +12,10 @@ const Login = () => {
   };
   const [username,setUsername]=useState("");
   const [password,setPassword]=useState('');
-
+  const {login,loading}=useLogin()
   const handleSubmit=async(e)=>{
     e.preventDefault();
-    await Login(username,password)
+    await login(username,password)
   }
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto text-gray-700 ">
@@ -22,13 +23,15 @@ const Login = () => {
         <h1 className="text-3xl font-semibold text-center text-gray-200">
           Log In
         </h1>
-        <form className=" ">
+        <form onSubmit={handleSubmit} className=" ">
           <div className="flex flex-col mb-4 ">
             <label className="p-2 font-semibold text-blue-800 ">Username</label>
             <input
               type="text"
               placeholder="Enter username "
               className="w-full input input-bordered h-10 bg-gray-900 rounded-md p-2"
+              value={username}
+              onChange={(e)=>setUsername(e.target.value)}
             />
           </div>
           <div className="flex flex-col mb-4 ">
@@ -37,6 +40,8 @@ const Login = () => {
               type="Password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10 bg-gray-900 rounded-md p-2"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
             />
           </div>
           <a
@@ -46,8 +51,10 @@ const Login = () => {
             Don't have an account?
           </a>
           <div className="flex items-cennter justify-center">
-            <button className="font-semibold text-blue-800 bg-transparent my-4 border border-slate-700 px-2 w-100 py-2 rounded-md hover:cursor-pointer">
-              Log In
+            <button 
+            disabled={loading}
+            className="font-semibold text-blue-800 bg-transparent my-4 border border-slate-700 px-2 w-100 py-2 rounded-md hover:cursor-pointer">
+              {loading?"Logging In":"Log In"}
             </button>
           </div>
         </form>
